9f5608f2eb84683d927eb63cd59197b678859c49
Tharindu Sandaruwan <sdtharindusandaruwan123@gmail.com>
2024-05-08 16:01:05 +0530

    8/5

---
+++ frontend/src/context/workoutsContext.jsx
@@ -0,0 +1,28 @@
+import { createContext } from "react";
+
+export const WorkoutsContext = createContext();
+
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUT":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workout],
+      };
+    default:
+      return state;
+  }
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, { workouts: null });
+
+  return (
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider>
+  );
+};
