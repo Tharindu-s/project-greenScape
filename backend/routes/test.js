724c1414dfb4d6752b368656c9c902b0d156ca8c
Tharindu Sandaruwan <sdtharindusandaruwan123@gmail.com>
2024-07-12 14:13:53 +0530

    removed backend

--- backend/routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createTest } = require("../controllers/testController");
-
-const router = express.Router();
-
-// POST a new product
-router.post("/", createTest);
-
-module.exports = router;
