0fb0c27f3d9a90b7db7641a3ab9d2d204bd80493
Tharindu Sandaruwan <sdtharindusandaruwan123@gmail.com>
2024-07-24 12:40:23 +0530

    backup

--- frontend/src/app/test/page.jsx
+++ frontend/src/app/test/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Dialog,
@@ -11,57 +12,58 @@ import {
 const page = () => {
   return (
     <div className="mt-32">
-      <section className="py-24 ">
-        <div className="w-full px-4 mx-auto max-w-7xl md:px-5 lg-6">
-          <div className="grid grid-cols-1 gap-8">
-            <div className="grid max-w-sm grid-cols-12 mx-auto sm:max-w-full">
-              <div className="col-span-12 lg:col-span-10 ">
-                <div className="gap-6 sm:flex">
-                  <img
-                    src="https://pagedone.io/asset/uploads/1704364560.png"
-                    alt="Robert image"
-                    className="w-32 h-32 rounded-full"
-                  />
-                  <div className="text">
-                    <p className="mt-6 mb-2 text-lg font-medium leading-8 text-gray-900 sm:mt-0">
-                      Robert Karmazov
-                    </p>
-
-                    <p className="text-base font-medium leading-8 text-gray-900 ">
-                      Very satisfied
-                    </p>
-
-                    <p className="mb-4 text-base font-normal leading-7 text-gray-400 lg:pr-8 ">
-                      description
-                    </p>
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center gap-2">
-                        <Dialog>
-                          <DialogTrigger>View more</DialogTrigger>
-                          <DialogContent className="w-[300px] md:w-[500px] rounded-xl">
-                            <DialogHeader>
-                              <DialogDescription className="pt-4">
-                                <p className="mb-4 text-base font-normal leading-7 text-gray-900">
-                                  description
-                                </p>
-                              </DialogDescription>
-                            </DialogHeader>
-                          </DialogContent>
-                        </Dialog>
-                      </div>
-                      <p className="text-sm font-medium leading-7 text-gray-400 lg:hidden lg:text-center whitespace-nowrap">
-                        Nov 01, 2023
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-row col-span-12 pt-12 lg:col-span-2 max-lg:hidden lg:flex-col max-lg:pt-6 ">
-                <p className="text-lg font-medium leading-8 text-gray-400 lg:text-center whitespace-nowrap">
-                  Nov 01, 2023
+      <section className="relative flex justify-center">
+        <img
+          src="https://pagedone.io/asset/uploads/1702362010.png"
+          alt="gradient background image"
+          className="fixed object-cover w-full h-full"
+        />
+        <div className="absolute max-w-lg px-6 py-20 mx-auto lg:px-8">
+          <img
+            src="https://pagedone.io/asset/uploads/1702362108.png"
+            alt="pagedone logo"
+            className="mx-auto mb-8 lg:mb-11"
+          />
+          <div className="bg-white shadow-xl rounded-2xl">
+            <form action="" className="mx-auto lg:p-11 p-7">
+              <div className="mb-11">
+                <h1 className="mb-2 text-3xl font-bold leading-10 text-center text-gray-900 font-manrope">
+                  Welcome Back
+                </h1>
+                <p className="text-base font-medium leading-6 text-center text-gray-500">
+                  Let’s get started with your 30 days free trial
                 </p>
               </div>
-            </div>
+              <input
+                type="text"
+                className="w-full h-12 px-4 mb-6 text-lg font-normal leading-7 text-gray-900 border border-gray-300 rounded-full shadow-sm placeholder:text-gray-400 focus:outline-none"
+                placeholder="Username"
+              />
+              <input
+                type="password"
+                className="w-full h-12 px-4 mb-1 text-lg font-normal leading-7 text-gray-900 border border-gray-300 rounded-full shadow-sm placeholder:text-gray-400 focus:outline-none"
+                placeholder="Password"
+              />
+              <a href="#" className="flex justify-end mb-6">
+                <span className="text-base font-normal leading-6 text-right text-indigo-600">
+                  Forgot Password?
+                </span>
+              </a>
+              <button className="w-full h-12 text-base font-semibold leading-6 text-center text-white transition-all duration-700 bg-indigo-600 rounded-full shadow-sm hover:bg-indigo-800 mb-11">
+                Login
+              </button>
+              <a
+                href="#"
+                className="flex justify-center text-base font-medium leading-6 text-gray-900"
+              >
+                {" "}
+                Don’t have an account?{" "}
+                <span className="pl-3 font-semibold text-indigo-600">
+                  {" "}
+                  Sign Up
+                </span>
+              </a>
+            </form>
           </div>
         </div>
       </section>
--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { ImLinkedin } from "react-icons/im";
 import { IoLogoWhatsapp } from "react-icons/io";
