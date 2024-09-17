40e52d7588194c5738b833411b92b0b743f4bcc1
Tharindu Sandaruwan <sdtharindusandaruwan123@gmail.com>
2024-09-17 19:11:23 +0530

    before blog

---
+++ backend/models/blogModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: [String],
+      required: true,
+    },
+    condition: {
+      sell: {
+        type: Boolean,
+        default: false,
+      },
+      exchange: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    isProfessional: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
