698908ad9e278007c477889cf2562c0f2bf80b24
Tharindu Sandaruwan <sdtharindusandaruwan123@gmail.com>
2024-10-23 17:10:19 +0530

    update server,js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,78 @@
+// require("dotenv").config();
+// const cors = require("cors");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// mongoose.set("strictQuery", false);
+
+// // routes
+// const userRoutes = require("./routes/user");
+// const productRoutes = require("./routes/product");
+// const reviewRoutes = require("./routes/reviews");
+// const professionalRoutes = require("./routes/professional");
+// const projectRoutes = require("./routes/project");
+// const testRoutes = require("./routes/test");
+// const exchangeRoutes = require("./routes/exchange");
+// const conversationRoutes = require("./routes/conversation");
+// const messageRoutes = require("./routes/message");
+// const inquiryRoutes = require("./routes/inquiry");
+// const cartRoutes = require("./routes/cart");
+// const serviceRoutes = require("./routes/service");
+// const blogRoutes = require("./routes/blogs");
+// const adminRoutes = require("./routes/admin");
+// const reportProductsRoutes = require("./routes/reportProduct");
+// const reportBlogsRoutes = require("./routes/reportBlog");
+// const sellerRatingsRoutes = require("./routes/rateSellers");
+
+// // express app
+// const app = express();
+// app.use(cors());
+
+// // middleware
+// app.use(express.json());
+
+// app.use((req, res, next) => {
+//   console.log(req.path, req.method);
+//   next();
+// });
+
+// app.get("/", (req, res) => {
+//   res.send(
+//     "<h1>Welcome to GreenScape backend</h1><p>The server is runningup and running</p>"
+//   );
+// });
+
+// // routes
+// app.use("/api/user", userRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/reviews", reviewRoutes);
+// app.use("/api/professional", professionalRoutes);
+// app.use("/api/projects", projectRoutes);
+// app.use("/api/test", testRoutes);
+// app.use("/api/exchange", exchangeRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/conversation", conversationRoutes);
+// app.use("/api/inquiry", inquiryRoutes);
+// app.use("/api/cart", cartRoutes);
+// app.use("/api/service", serviceRoutes);
+// app.use("/api/blogs", blogRoutes);
+// app.use("/api/admin", adminRoutes);
+// app.use("/api/reportproduct", reportProductsRoutes);
+// app.use("/api/reportblog", reportBlogsRoutes);
+// app.use("/api/sellerrating", sellerRatingsRoutes);
+
+// // connect to db
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     // listen for requests
+//     app.listen(process.env.PORT, () => {
+//       console.log("connected to db & listening on port", process.env.PORT);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
@@ -35,10 +110,9 @@ app.use((req, res, next) => {
   next();
 });
 
+// A simple route to show a message on root URL
 app.get("/", (req, res) => {
-  res.send(
-    "<h1>Welcome to GreenScape backend</h1><p>The server is runningup and running</p>"
-  );
+  res.send("<h1>Welcome to the Express App!</h1><p>The server is running.</p>");
 });
 
 // routes
@@ -64,11 +138,11 @@ app.use("/api/sellerrating", sellerRatingsRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log("connected to db & listening on port", process.env.PORT);
-    });
+    console.log("connected to db");
   })
   .catch((error) => {
     console.log(error);
   });
+
+// Export the app for Vercel
+module.exports = app;
